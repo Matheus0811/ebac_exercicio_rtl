b93b4504bfee9fc4dfe9767faee822f9f4fa11c1
Matheus0811 <matheusbg0811@gmail.com>
2025-03-27 15:07:19 -0300

    foi

---
+++ src/components/PostComments/PostComments.test.tsx
@@ -0,0 +1,10 @@
+export {};
+import { fireEvent, render, screen } from '@testing-library/react';
+import PostComment from '.';
+
+describe('Teste para o componente PostComment', () => {
+    it('Deve renderizar o componente corretamente', () => {
+        render(<PostComment/>);
+        expect(screen.getByText('Comentar')).toBeInTheDocument();
+    });
+});
