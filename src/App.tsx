bfdd50537c5152ccb2ad84d44bf0e41f4b774671
Matheus0811 <matheusbg0811@gmail.com>
2025-03-27 15:42:53 -0300

    fennec

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,8 @@ import styles from './App.module.css';
 function App() {
   return (
     <div className={styles.app}>
-      <Post imageUrl="https://www.orangeboxminiaturas.com.br/img/products/batmovel-1989-figura-batman-em-metal-jada-toys-1-24-jad-98260_1_1000.jpg">
-        Olha só que legal minha miniatura do Batmóvel.
+      <Post imageUrl="https://preview.redd.it/1amul35hrbi71.jpg?width=1080&crop=smart&auto=webp&s=4f64675ea7b18d2e34f68c30af39254d11edd3a7"> <hr />
+        Olha só que legal esse Fennec
       </Post>
     </div>
   );
